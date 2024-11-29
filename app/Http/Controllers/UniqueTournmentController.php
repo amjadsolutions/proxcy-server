7e7b0193bc2ac72abbabd8cfdf59f452cc01573b
amjadsolutions <amjadjavedse@gmail.com>
2024-11-29 15:06:05 +0400

    updated code

---
+++ app/Http/Controllers/UniqueTournmentController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Http;
+
+class UniqueTournmentController extends Controller
+{
+    public function getSeasons($league)
+    {
+
+        $response = Http::withoutVerifying()->get("https://sofascore.com/api/v1/unique-tournament/" . $leagueId . "/seasons");
+        // Fetch data from external API
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+        // Handle errors
+        return response()->json(['error' => 'Unable to fetch data'], $response->status());
+    }
+
+    public function getSeasonNextMatches($leagueId, $seasonId)
+    {
+        // If data does not exist in Redis, fetch from API
+        $response = Http::withoutVerifying()->get("https://www.sofascore.com/api/v1/unique-tournament/" . $leagueId . "/season/" . $seasonId . "/events/next/0");
+
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+        // Handle errors
+        return response()->json(['error' => 'Unable to fetch data'], $response->status());
+    }
+
+    public function getSeasonPreviousMatches($leagueId, $seasonId)
+    {
+        // If data does not exist in Redis, fetch from API
+        $response = Http::withoutVerifying()->get("https://www.sofascore.com/api/v1/unique-tournament/" . $leagueId . "/season/" . $seasonId . "/events/last/0");
+
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+        // Handle errors
+        return response()->json(['error' => 'Unable to fetch data'], $response->status());
+    }
+
+    public function getSeasonStandings($leagueId, $seasonId)
+    {
+
+        // If data does not exist in Redis, fetch from API
+        $response = Http::withoutVerifying()->get("https://sofascore.com/api/v1/unique-tournament/" . $leagueId . "/season/" . $seasonId . "/standings/total");
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+        // Handle errors
+        return response()->json(['error' => 'Unable to fetch data'], $response->status());
+
+    }
+
+    public function getSeasonPlayerStats($leagueId, $seasonId)
+    {
+
+        // If data does not exist in Redis, fetch from API
+        $response = Http::withoutVerifying()->get("https://www.sofascore.com/api/v1/unique-tournament/" . $leagueId . "/season/" . $seasonId . "/top-players/overall");
+
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+        // Handle errors
+        return response()->json(['error' => 'Unable to fetch data'], $response->status());
+
+    }
+
+    public function topLeagueDetails($leagueId)
+    {
+
+        // Fetch data from the API if not found in Redis
+        $response = Http::withoutVerifying()->get("https://sofascore.com/api/v1/unique-tournament/" . $leagueId);
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+        // Handle errors
+        return response()->json(['error' => 'Unable to fetch data'], $response->status());
+
+    }
+
+    public function getLeagueContents($leagueId)
+    {
+
+        // Fetch data from the API if not found in Redis
+        $response = Http::withoutVerifying()->get("https://sofascore.com/api/v1/seo/content/unique-tournament/" . $leagueId . "/en");
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+        // Handle errors
+        return response()->json(['error' => 'Unable to fetch data'], $response->status());
+    }
+
+    public function getFeaturedMatch($leagueId)
+    {
+
+        // If data does not exist in Redis, fetch from API
+        $response = Http::withoutVerifying()->get("https://www.sofascore.com/api/v1/unique-tournament/" . $leagueId . "/featured-events");
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+        // Handle errors
+        return response()->json(['error' => 'Unable to fetch data'], $response->status());
+
+    }
+
+    public function getSeasonInfo($leagueId, $seasonId)
+    {
+
+        // If data does not exist in Redis, fetch from API
+        $response = Http::withoutVerifying()->get("https://www.sofascore.com/api/v1/unique-tournament/" . $leagueId . "/season/" . $seasonId . "/info");
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+        // Handle errors
+        return response()->json(['error' => 'Unable to fetch data'], $response->status());
+
+    }
+
+
+   
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DailyMatchesController;  // Import the controller
+use App\Http\Controllers\DailyMatchesController;
+use App\Http\Controllers\UniqueTournmentController;
+use Illuminate\Http\Request; // Import the controller
 
+use Illuminate\Support\Facades\Route;
+// Import the controller
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +16,27 @@ use App\Http\Controllers\DailyMatchesController;  // Import the controller
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('v1')->group(function () {
 
-Route::get('/v1/sport/cricket/scheduled-events/{date}', [DailyMatchesController::class, 'getDailyMatches']);
+    // matches endpoint
+    Route::get('/sport/cricket/scheduled-events/{date}', [DailyMatchesController::class, 'getDailyMatches']);
 
+
+    // league endpoints
+    Route::get('/unique-tournament/{leagueId}/season/{seasonId}/info', [UniqueTournmentController::class, 'getSeasonInfo']);
+    Route::get('/unique-tournament/{leagueId}/featured-events', [UniqueTournmentController::class, 'getFeaturedMatch']);
+    Route::get('/seo/content/unique-tournament/{leagueId}/en");', [UniqueTournmentController::class, 'getLeagueContents']);
+    Route::get('/unique-tournament/{leagueId}/season/{seasonId}/top-players/overall"', [UniqueTournmentController::class, 'getSeasonPlayerStats']);
+    Route::get('/unique-tournament/{leagueId}', [UniqueTournmentController::class, 'topLeagueDetails']);
+    Route::get('/unique-tournament/{leagueId}/season/seasonId/standings/total', [UniqueTournmentController::class, 'getSeasonStandings']);
+    Route::get('/unique-tournament/{leagueId}/season/{seasonId}/events/last/0', [UniqueTournmentController::class, 'getSeasonPreviousMatches']);
+    Route::get('/unique-tournament/{leagueId}/season/{seasonId}/events/next/0', [UniqueTournmentController::class, 'getSeasonNextMatches']);
+    Route::get('/unique-tournament/{leagueId}/seasons', [UniqueTournmentController::class, 'getSeasons']);
+
+});
