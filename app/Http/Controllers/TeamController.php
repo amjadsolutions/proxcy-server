15139555faea9283208d49cd8279c969877d9beb
amjadsolutions <amjadjavedse@gmail.com>
2024-11-29 16:52:23 +0400

    updated code

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -45,6 +45,9 @@ class TeamController extends Controller
 
     public function getTeamSquad($teamId)
     {
+        dd('aa');
+
+        dd('a');
 
         $response = Http::withoutVerifying()->get("https://sofascore.com/api/v1/team/" . $teamId . "/players");
         if ($response->successful()) {
