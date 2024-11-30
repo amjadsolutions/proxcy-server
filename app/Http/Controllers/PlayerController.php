b40b27fe3efd46eb361326ab91a2f647c0541f54
amjadsolutions <amjadjavedse@gmail.com>
2024-11-30 20:19:16 +0400

    updated code

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -21,17 +21,6 @@ class PlayerController extends Controller
     }
 
 
-    public function getTeamMatchH2H1($customId)
-    {
-
-        $responseContents = Http::withoutVerifying()->get("https://sofascore.com/api/v1/seo/content/player/" . $playerId . "/en");
-        if ($response->successful()) {
-            return response()->json($response->json());
-        }
-        // Handle errors
-        return response()->json(['error' => 'Unable to fetch data'], $response->status());
-
-    }
 
 
 
